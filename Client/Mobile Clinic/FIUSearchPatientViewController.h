979b3b9ec6fd16f6bf399c7fce3e6ae9c89cc94a
Rigo Hernandez <rigohernandez@gmail.com>
2013-02-10 13:36:15 -0500

    Reimplemented new search view.

--- Client/Mobile Clinic/FIUSearchPatientViewController.h
+++ Client/Mobile Clinic/FIUSearchPatientViewController.h
@@ -10,4 +10,11 @@
 
 @interface FIUSearchPatientViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *patientNameField;
+- (IBAction)searchByNameButton:(id)sender;
+- (IBAction)searchByNFCButton:(id)sender;
+- (IBAction)searchByFingerprintButton:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *patientResultTableView;
+
+
 @end
--- Client/Mobile Clinic/FIUSearchPatientViewController.m
+++ Client/Mobile Clinic/FIUSearchPatientViewController.m
@@ -8,6 +8,10 @@
 
 #import "FIUSearchPatientViewController.h"
 
+//Global Variable Declarations
+FIUAppDelegate *appDelegate;
+NSMutableArray *patientResultsArray;
+
 @interface FIUSearchPatientViewController ()
 
 @end
@@ -27,6 +31,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    // Instantiate Global Variables
+    appDelegate = (FIUAppDelegate *)[[UIApplication sharedApplication] delegate];
+    patientResultsArray = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +43,73 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setPatientNameField:nil];
+    [self setPatientResultTableView:nil];
+    [super viewDidUnload];
+}
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+// Determines the number of rows that appear in the table view
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return patientResultsArray.count;
+}
+
+//
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] ;
+    }
+    
+    NSManagedObject * obj = [patientResultsArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [obj valueForKey:@"firstname"];
+    //    cell.textLabel.text = [obj valueForKey:@"family_name"];
+    cell.textLabel.text = [obj valueForKey:@"age"];
+    //    cell.textLabel.text = [obj valueForKey:@"sex"];
+    
+    return cell;
+}
+
+//
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+// Search manually by patient name
+- (IBAction)searchByNameButton:(id)sender {
+    
+    NSError *error;
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    
+    [request setEntity:[NSEntityDescription entityForName:@"Patients" inManagedObjectContext:context]];
+    [request setPredicate:[NSPredicate predicateWithFormat:
+                           @"(firstname contains[cd] %@) OR (family_name contains[cd] %@)", _patientNameField.text, _patientNameField.text]];
+    
+    patientResultsArray = [NSMutableArray arrayWithArray:[context executeFetchRequest:request error:&error]];
+    [_patientResultTableView reloadData];
+    
+    for(NSManagedObject *patients in patientResultsArray){
+        NSLog(@"Name: %@", [patients valueForKey:@"firstname"]);
+        NSLog(@"Age: %@", [patients valueForKey:@"age"]);
+        NSLog(@"Sex: %@", [patients valueForKey:@"sex"]);
+    }
+}
+
+- (IBAction)searchByNFCButton:(id)sender {
+}
+
+- (IBAction)searchByFingerprintButton:(id)sender {
+}
 @end
