fcd2d78d1f22de360c7a8140b305d3b2ce29a773
SZanlongo <SZanlongo@users.noreply.github.com>
2013-02-19 14:02:49 -0500

    Added old search code from diagnosis

--- Client/Mobile Clinic/FIUDiagnosisViewController.m
+++ Client/Mobile Clinic/FIUDiagnosisViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,12 +44,87 @@
     [self setPatientWeightLabel:nil];
     [self setPatientSexLabel:nil];
     [self setVillageNameLabel:nil];
-    [self setSymptomsText:nil];
     [self setDiagnosisText:nil];
-    [self setTreatmentsText:nil];
-    [self setCancelButton:nil];
-    [self setCheckoutButton:nil];
-    [self setSubmitButton:nil];
+    [self setSearchButton:nil];
     [super viewDidUnload];
 }
+
+- (IBAction)checkoutButton:(id)sender {
+    id navCtrl = [self getViewControllerFromiPadStoryboardWithName:@"mainNavigationController"];
+    [self presentViewController:navCtrl animated:YES completion:^{
+        [FIUAppDelegate getNotificationWithColor:AJNotificationTypeGreen Animation:AJLinedBackgroundTypeAnimated WithMessage:@"Patient Checked Out"];
+    }];
+}
+
+-(void)setupView:(UIViewController<UIPopoverControllerDelegate>*)vc ForPopoverByButton:(id)btn hasController:(id)controller{
+    
+    id ctrl;
+    
+    if (controller) {
+        ctrl = controller;
+    }else{
+        ctrl = vc;
+    }
+    
+    popover = [[UIPopoverController alloc]initWithContentViewController:ctrl];
+    
+    [popover setDelegate:vc];
+    
+    if ([btn isKindOfClass:[UIButton class]]) {
+        UIButton* button = btn;
+        [popover presentPopoverFromRect:button.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }else{
+        UIBarButtonItem* button = btn;
+        [popover presentPopoverFromBarButtonItem:button permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
+- (IBAction)searchForPatients:(id)sender {
+    
+    FIUPatientRegistrationViewController* vc = [self getViewControllerFromiPadStoryboardWithName:@"SearchPatients"];
+    [_searchButton setEnabled:NO];
+    [vc setScreenHandler:^(id object, NSError *error) {
+        //Should check for when the error is true
+        [popover dismissPopoverAnimated:YES];
+        [_searchButton setEnabled:YES];
+        // if there was no error and there is an object
+        // redisplay information
+        if (object) {
+            //Redisplay Method here
+            _patient = object;
+            [self Redisplay];
+        }
+    }];
+    
+    [self setupView:vc ForPopoverByButton:sender hasController:nil];
+}
+
+-(void)Redisplay{
+    //set picture
+    [_patientPicture setImage:_patient.patient.photo];
+    
+    _familyNameLabel.text = _patient.patient.familyName;
+    _patientNameLabel.text = _patient.patient.firstName;
+    _villageNameLabel.text = _patient.patient.villageName;
+    
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"MM-dd-yyy"];
+    _patientAgeLabel.text = [dateFormat stringFromDate:_patient.patient.age];
+    
+    if (_patient.patient.sex = 0) {
+        _patientSexLabel.text = @"Female";
+    } else {
+        _patientSexLabel.text = @"Male";
+    }
+    
+}
+
+- (IBAction)submitButton:(id)sender {
+    
+    id navCtrl = [self getViewControllerFromiPadStoryboardWithName:@"mainNavigationController"];
+    [self presentViewController:navCtrl animated:YES completion:^{
+        [FIUAppDelegate getNotificationWithColor:AJNotificationTypeGreen Animation:AJLinedBackgroundTypeAnimated WithMessage:@"Patient Diagnosis Submitted"];
+    }];
+}
+
 @end
