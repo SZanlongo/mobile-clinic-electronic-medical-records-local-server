856e6cbc8d5aaee366832ce6e5fb1489f0ee6f87
SZanlongo <SZanlongo@users.noreply.github.com>
2013-02-22 12:07:23 -0500

    added doctor and pharmacy view controllers

---
+++ Client/Mobile Clinic/Current DiagnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  Current DiagnosisViewController.h
+//  Mobile Clinic
+//
+//  Created by sebastian a zanlongo on 2/22/13.
+//  Copyright (c) 2013 Steven Berlanga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Current_DiagnosisViewController : UIViewController
+
+@end
---
+++ Client/Mobile Clinic/DoctorPatientViewController.h
@@ -0,0 +1,13 @@
+//
+//  DoctorPatientViewController.h
+//  Mobile Clinic
+//
+//  Created by sebastian a zanlongo on 2/22/13.
+//  Copyright (c) 2013 Steven Berlanga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoctorPatientViewController : UIViewController
+
+@end
