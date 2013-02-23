8e0330ab6a98292350ee3ef5f2a1743061711e48
SZanlongo <SZanlongo@users.noreply.github.com>
2013-02-23 11:23:02 -0500

    Added new views to storyboard. Color-coded all stations. Began work on doctor tableviews

--- Client/Mobile Clinic/DoctorViewController.h
+++ Client/Mobile Clinic/DoctorViewController.h
@@ -2,24 +2,29 @@
 //  DoctorViewController.h
 //  Mobile Clinic
 //
-//  Created by sebastian a zanlongo on 2/22/13.
+//  Created by sebastian a zanlongo on 2/18/13.
 //  Copyright (c) 2013 Steven Berlanga. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "RegisterPatientTableCell.h"
 #import "SearchPatientTableCell.h"
+//#import "SearchPatientViewController.h"
 #import "StationViewHandlerProtocol.h"
-
 @interface DoctorViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>{
     ScreenHandler handler;
 }
 
+
+
 @property (strong, nonatomic) SearchPatientViewController * viewController;
 @property (strong, nonatomic) PatientObject * patientData;
+@property (strong, nonatomic) RegisterPatientViewController * control1;
 @property (strong, nonatomic) SearchPatientViewController * control2;
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
 
 -(void)setScreenHandler:(ScreenHandler)myHandler;
-
+@property (nonatomic, retain) IBOutlet UISegmentedControl *segmentedControl;
 @end
--- Client/Mobile Clinic/DoctorViewController.m
+++ Client/Mobile Clinic/DoctorViewController.m
@@ -2,17 +2,20 @@
 //  DoctorViewController.m
 //  Mobile Clinic
 //
-//  Created by sebastian a zanlongo on 2/22/13.
+//  Created by sebastian a zanlongo on 2/18/13.
 //  Copyright (c) 2013 Steven Berlanga. All rights reserved.
 //
 
 #import "DoctorViewController.h"
+#import "DoctorPatientViewController.h"
 
 @interface DoctorViewController ()
 
 @end
 
-@implementation DoctorViewController
+@implementation TriageViewController
+
+@synthesize segmentedControl;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,7 +30,58 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    UINavigationBar *bar =[self.navigationController navigationBar];
+    [bar setTintColor:[UIColor orangeColor]];
+    
+    // Rotate table horizontally (90 degrees)
+    CGAffineTransform transform = CGAffineTransformMakeRotation(-1.5707963);
+    _tableView.rowHeight = 768;
+    _tableView.transform = transform;
+    [_tableView setShowsVerticalScrollIndicator:NO];
+    
+    // Create controllers for each view (Search & Register)
+    //    _control2 = [([UIStoryboard storyboardWithName:@"NewStoryboard" bundle: nil]) instantiateViewControllerWithIdentifier:@"searchPatientViewController"];
+    
+    _control1 = [self getViewControllerFromiPadStoryboardWithName:@"registerPatientViewController"];
+    _control2 = [self getViewControllerFromiPadStoryboardWithName:@"searchPatientViewController"];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createPatient:) name:CREATE_NEW_PATIENT object:_patientData];
+    //    if([_control1 view])
+    //        [_control1.createPatientButton addTarget:self action:@selector(createPatient) forControlEvents:UIControlEventTouchUpInside];
+    
+    if([_control2 view])
+        [_control2.patientFound addTarget:self action:@selector(searchPatient) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    //    UIBarButtonItem * backButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRewind target:self.navigationController.self action:@selector(popToRootViewControllerAnimated:)];
+    //
+    //    [self.navigationItem setLeftBarButtonItem:backButton];
+    
 }
+
+//-(void)back{
+//    [self.navigationController popToRootViewControllerAnimated:YES]
+//}
+
+-(void)createPatient:(NSNotification *)note {
+    _patientData = note.object;
+    
+    TriagePatientViewController *newView = [self getViewControllerFromiPadStoryboardWithName:@"triagePatientViewController"];
+    
+    newView.patientData = _patientData;
+    
+    [self.navigationController pushViewController:newView animated:YES];
+    
+    //    PatientObject * newPatient = [_control1 createPatient];
+    //    [self performSegueWithIdentifier:@"triagePatientViewController" sender:_patientData];
+}
+
+-(void)searchPatient{
+    PatientObject * newPatient = _control2.patientData;
+    [self performSegueWithIdentifier:@"triagePatientViewController" sender:newPatient];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -35,44 +89,85 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setTableView:nil];
+    [self setToolBar:nil];
+    [super viewDidUnload];
+}
+
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return 2;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString * registerCellIdentifier = @"registerCell";
     static NSString * searchCellIdentifier = @"searchCell";
-    SearchPatientTableCell * cell = [tableView dequeueReusableCellWithIdentifier:searchCellIdentifier];
     
-    if(!cell){
-        cell = [[SearchPatientTableCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:searchCellIdentifier];
+    if(indexPath.item == 0) {
+        RegisterPatientTableCell * cell = [tableView dequeueReusableCellWithIdentifier:registerCellIdentifier];
         
-        cell.viewController = _control2;
+        if(!cell){
+            cell = [[RegisterPatientTableCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:registerCellIdentifier];
+            
+            cell.viewController = _control1;
+        }
+        
+        CGAffineTransform transform = CGAffineTransformMakeRotation(1.5707963);
+        cell.viewController.view.transform = transform;
+        cell.viewController.view.frame = CGRectMake(0, 0, 916, 768);
+        
+        for(UIView *mView in [cell.contentView subviews]){
+            [mView removeFromSuperview];
+        }
+        
+        [cell addSubview: cell.viewController.view];
+        [cell.viewController setScreenHandler:handler];
+        return cell;
     }
-    
-    CGAffineTransform transform = CGAffineTransformMakeRotation(1.5707963);
-    cell.viewController.view.transform = transform;
-    cell.viewController.view.frame = CGRectMake(0, 0, 916, 768);
-    
-    for(UIView *mView in [cell.contentView subviews]){
-        [mView removeFromSuperview];
+    else {
+        SearchPatientTableCell * cell = [tableView dequeueReusableCellWithIdentifier:searchCellIdentifier];
+        
+        if(!cell){
+            cell = [[SearchPatientTableCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:searchCellIdentifier];
+            
+            cell.viewController = _control2;
+        }
+        
+        CGAffineTransform transform = CGAffineTransformMakeRotation(1.5707963);
+        cell.viewController.view.transform = transform;
+        cell.viewController.view.frame = CGRectMake(0, 0, 916, 768);
+        
+        for(UIView *mView in [cell.contentView subviews]){
+            [mView removeFromSuperview];
+        }
+        
+        [cell addSubview: cell.viewController.view];
+        
+        return cell;
     }
-    
-    [cell addSubview: cell.viewController.view];
-    
-    return cell;
-    
 }
 
 -(void)setScreenHandler:(ScreenHandler)myHandler{
     handler = myHandler;
 }
-
-- (void)viewDidUnload {
-    [self setTableView:nil];
-    [super viewDidUnload];
+- (IBAction) segmentedControlIndexChanged {
+    switch (self.segmentedControl.selectedSegmentIndex) {
+        case 0:
+            [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:YES];
+            break;
+        case 1:
+            [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForItem:1 inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:YES];
+            break;
+        default:
+            break;
+            
+    }
+    [self.tableView reloadData];
+    
 }
+
 @end
