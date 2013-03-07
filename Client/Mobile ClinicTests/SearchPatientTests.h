5e50dcdb6279fcfd2245262ef20b3b56133783be
Rigo Hernandez <rigohernandez@gmail.com>
2013-03-07 13:01:45 -0500

    Added test class for search.

---
+++ Client/Mobile ClinicTests/SearchPatientTests.h
@@ -0,0 +1,13 @@
+//
+//  SearchPatientTests.h
+//  Mobile Clinic
+//
+//  Created by Rigo Hernandez on 3/5/13.
+//  Copyright (c) 2013 Steven Berlanga. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SearchPatientTests : SenTestCase
+
+@end
---
+++ Client/Mobile ClinicTests/SearchPatientTests.m
@@ -0,0 +1,13 @@
+//
+//  SearchPatientTests.m
+//  Mobile Clinic
+//
+//  Created by Rigo Hernandez on 3/5/13.
+//  Copyright (c) 2013 Steven Berlanga. All rights reserved.
+//
+
+#import "SearchPatientTests.h"
+
+@implementation SearchPatientTests
+
+@end
