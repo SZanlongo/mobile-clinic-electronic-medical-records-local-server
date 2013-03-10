1e6c7ff3223354f80cb4db224610f2fa6f59e3d4
Rigo Hernandez <rigohernandez@gmail.com>
2013-03-10 17:44:40 -0400

    Created new patient queue for doctor.  Implemented logic to populate cells with queued patients info.  Sorted patient's by priority.  Need to test implementation.

--- Client/Mobile Clinic/PatientQueueViewController.h
+++ Client/Mobile Clinic/PatientQueueViewController.h
@@ -2,12 +2,22 @@
 //  PatientQueueViewController.h
 //  Mobile Clinic
 //
-//  Created by sebastian a zanlongo on 3/7/13.
+//  Created by Rigo Hernandez on 3/10/13.
 //  Copyright (c) 2013 Steven Berlanga. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "MobileClinicFacade.h"
 
-@interface PatientQueueViewController : UIViewController
+@interface PatientQueueViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *queueTableView;
+
+@end
+
+@interface QueueTableCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *patientName;
+@property (weak, nonatomic) IBOutlet UILabel *patientDOB;
+@property (weak, nonatomic) IBOutlet UILabel *patientAge;
+@property (weak, nonatomic) IBOutlet UIImageView *patientPhoto;
 
 @end
