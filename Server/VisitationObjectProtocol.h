58a627cd516c1697ad85b6b770e130e0d347e9af
Rigo Hernandez <rigo@Rigos-MacBook-Air.local>
2013-04-14 02:39:43 -0400

    Added Assessment to the database and corrected some alignment problems with the menu.xib.

--- Server/VisitationObjectProtocol.h
+++ Server/VisitationObjectProtocol.h
@@ -5,16 +5,17 @@
 //  Created by Michael Montaque on 3/10/13.
 //  Copyright (c) 2013 Florida International University. All rights reserved.
 //
-#define TRIAGEIN    @"triageIn"
-#define TRIAGEOUT   @"triageOut"
-#define DOCTORID    @"doctorId"
-#define PATIENTID   @"patientId"
-#define DOCTORIN    @"doctorIn"
-#define DOCTOROUT   @"doctorOut"
-#define CONDITION   @"condition"
+#define TRIAGEIN        @"triageIn"
+#define TRIAGEOUT       @"triageOut"
+#define DOCTORID        @"doctorId"
+#define PATIENTID       @"patientId"
+#define DOCTORIN        @"doctorIn"
+#define DOCTOROUT       @"doctorOut"
+#define ASSESSMENT      @"assessment"
+#define CONDITION       @"condition"
 #define CONDITIONTITLE  @"conditionTitle"
-#define DTITLE      @"diagnosisTitle"
-#define GRAPHIC     @"isGraphic"
+#define DTITLE          @"diagnosisTitle"
+#define GRAPHIC         @"isGraphic"
 #define WEIGHT          @"weight" //The different user types (look at enum)
 #define OBSERVATION     @"observation"
 #define NURSEID         @"nurseId"
